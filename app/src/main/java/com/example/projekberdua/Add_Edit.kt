18c4c345312a62d7dd31338c46a1064dce7a3512
jesyaprilia <jesyaprilia2@gmail.com>
2025-10-07 11:15:24 +0700

    ubah semua tampilan

--- app/src/main/java/com/example/projekberdua/Add_Edit.kt
+++ app/src/main/java/com/example/projekberdua/Add_Edit.kt
@@ -11,7 +11,7 @@ class Add_Edit : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_edit)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainn)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
